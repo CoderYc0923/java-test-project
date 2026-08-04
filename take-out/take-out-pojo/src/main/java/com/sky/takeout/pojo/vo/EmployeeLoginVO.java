9d7d0376df7fa13ba60747066d3a94551cb47d42
yechao <1151062047@qq.com>
2026-08-04 22:10:43 +0800

    feat: 接入swagger

--- take-out/take-out-pojo/src/main/java/com/sky/takeout/pojo/vo/EmployeeLoginVO.java
+++ take-out/take-out-pojo/src/main/java/com/sky/takeout/pojo/vo/EmployeeLoginVO.java
@@ -2,12 +2,18 @@ package com.sky.takeout.pojo.vo;
 
 import lombok.Data;
 import lombok.Builder;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "员工登录返回结果")
 @Data
 @Builder
 public class EmployeeLoginVO {
+    @Schema(description = "员工id", example = "1")
     private Long id;
+    @Schema(description = "用户名", example = "admin")
     private String username;
+    @Schema(description = "姓名", example = "张三")
     private String name;
+    @Schema(description = "token", example = "123456")
     private String token;
 }
