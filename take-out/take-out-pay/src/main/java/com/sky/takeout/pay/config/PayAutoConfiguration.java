431e284895d05e924df64073f11fea543da5e5c1
yechao <1151062047@qq.com>
2026-08-08 23:22:46 +0800

    feat:  支付中心配置类

--- take-out/take-out-pay/src/main/java/com/sky/takeout/pay/config/PayAutoConfiguration.java
+++ take-out/take-out-pay/src/main/java/com/sky/takeout/pay/config/PayAutoConfiguration.java
@@ -1,5 +1,16 @@
 package com.sky.takeout.pay.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 支付中心自动配置
+ * PayAutoConfiguration
+ * @EnableConfigurationProperties 启用配置属性
+ * @Configuration 配置类
+ */
+@Configuration
+@EnableConfigurationProperties(PayProperties.class)
 public class PayAutoConfiguration {
 
 }
--- take-out/take-out-pay/src/main/java/com/sky/takeout/pay/config/PayProperties.java
+++ take-out/take-out-pay/src/main/java/com/sky/takeout/pay/config/PayProperties.java
@@ -1,5 +1,29 @@
 package com.sky.takeout.pay.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+/**
+ * 支付中心配置属性
+ * PayProperties
+ */
+@ConfigurationProperties(prefix = "pay")
+@Data
 public class PayProperties {
 
+    /**
+     * 模拟支付的密钥
+     */
+    private String mockSectet;
+
+    /**
+     * 订单幂等性过期时间（秒）
+     */
+    private Long orderIdempotentTtlSeconds;
+
+    /**
+     * 支付锁过期时间（秒）
+     */
+    private Long payLockTtlSeconds;
 }
