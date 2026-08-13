8a3ad840b30c18016deb7537bae4e962a0219895
yechao <1151062047@qq.com>
2026-08-13 18:12:23 +0800

    feat: 订单支付消息生产者

--- take-out/take-out-pay/src/main/java/com/sky/takeout/pay/mq/OrderPaidProducer.java
+++ take-out/take-out-pay/src/main/java/com/sky/takeout/pay/mq/OrderPaidProducer.java
@@ -1,6 +1,13 @@
 package com.sky.takeout.pay.mq;
 
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sky.takeout.pay.config.TakeoutMqProperties;
 
 /**
  * 订单支付消息生产者
@@ -8,6 +15,17 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class OrderPaidProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderPaidProducer.class);
+
+    private final TakeoutMqProperties takeoutMqProperties;
+
+    private final RocketMQTemplate rocketMQTemplate;
+
+    public OrderPaidProducer(TakeoutMqProperties takeoutMqProperties, RocketMQTemplate rocketMQTemplate) {
+        this.rocketMQTemplate = rocketMQTemplate;
+        this.takeoutMqProperties = takeoutMqProperties;
+    }
 
 
     /**
@@ -17,6 +35,16 @@ public class OrderPaidProducer {
      * @param eventId     事件ID
      */
     public void send(String jsonPayload, Long orderId, String eventId) {
-        
+        String destination = takeoutMqProperties.getOrderPaidTopic() + ":" + takeoutMqProperties.getOrderPaidTag();
+
+        rocketMQTemplate.syncSend(
+            destination,
+            MessageBuilder.withPayload(jsonPayload)
+                .setHeader("KEYS", String.valueOf(orderId))  // 部分版本用 keys
+                .setHeader("eventId", eventId)
+                .build()
+        );
+
+        log.info("发送订单支付消息成功，destination={}, orderId={}, eventId={}", destination, orderId, eventId);
     }
 }
